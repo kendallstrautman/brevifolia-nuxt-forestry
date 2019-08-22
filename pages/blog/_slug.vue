2032be79ba79441efbe7d8a73773bfbfdd3b2011
kendallstrautman <kendallstrautman@gmail.com>
2019-08-22 14:58:05 -0700

    testing method version

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -12,7 +12,7 @@
     <div class="blog__body" v-html="post.html"></div>
     <div class="blog__footer">
       <h2 >Written By: {{ post.attributes.author }}</h2>
-      <NuxtLink :to="`/blog/${nextBlogPath}`">
+      <NuxtLink :to="`/blog/${nextBlogPathMethod()}`">
         <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" x="0px" y="0px" viewBox="0 0 26 26" enableBackground="new 0 0 26 26" >
           <path d="M23.021,12.294l-8.714-8.715l-1.414,1.414l7.007,7.008H2.687v2h17.213l-7.007,7.006l1.414,1.414l8.714-8.713  C23.411,13.317,23.411,12.685,23.021,12.294z"/>
         </svg>
@@ -41,6 +41,21 @@
         return nextPath
       } 
     },
+    methods: {
+      nextBlogPathMethod() {
+const firstBlogPath = this.sortedPaths[0]
+        console.log('in method!')
+        console.log(this.sortedPaths)
+        console.log(this.currentPath)
+        // if there's no 'next' path, return the first path
+        const nextPath = isNull(this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]) ? firstBlogPath : this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]
+        function isNull(item) {
+          return item === null || item === undefined
+        }
+        console.log(this.sortedPaths)
+        return nextPath
+      }
+    },
     // get the slug as a param to import the correct md file
     async asyncData({ params }) {
       try {
@@ -68,11 +83,11 @@
         console.log(sortedPosts)
         const sortedPaths = [] 
         sortedPosts.map(post => {
-          console.log(post)
+          console.log(post.attributes._meta.resourcePath)
           // clean up the path
           const relPath = post.attributes._meta.resourcePath.slice(86, -3)
           console.log(relPath)
-          sortedPaths.push(post.attributes._meta.resourcePath)
+          sortedPaths.push(relPath)
         })
         console.log(sortedPaths)
         return {
