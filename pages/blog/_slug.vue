320cecc63c585f523cd1eef3384e1519c98c9de6
kendallstrautman <kendallstrautman@gmail.com>
2019-08-22 15:22:42 -0700

    adjusted filter of the next path

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -12,7 +12,7 @@
     <div class="blog__body" v-html="post.html"></div>
     <div class="blog__footer">
       <h2 >Written By: {{ post.attributes.author }}</h2>
-      <NuxtLink :to="`/blog/${nextBlogPathMethod()}`">
+      <NuxtLink :to="`/blog/${nextBlogPath}`">
         <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" x="0px" y="0px" viewBox="0 0 26 26" enableBackground="new 0 0 26 26" >
           <path d="M23.021,12.294l-8.714-8.715l-1.414,1.414l7.007,7.008H2.687v2h17.213l-7.007,7.006l1.414,1.414l8.714-8.713  C23.411,13.317,23.411,12.685,23.021,12.294z"/>
         </svg>
@@ -30,31 +30,14 @@
       nextBlogPath() {
         const firstBlogPath = this.sortedPaths[0]
         console.log('in computed!')
-        console.log(this.sortedPaths)
-        console.log(this.currentPath)
         // if there's no 'next' path, return the first path
         const nextPath = isNull(this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]) ? firstBlogPath : this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]
         function isNull(item) {
           return item === null || item === undefined
         }
-        console.log(this.sortedPaths)
+        console.log(nextPath)
         return nextPath
       } 
-    },
-    methods: {
-      nextBlogPathMethod() {
-const firstBlogPath = this.sortedPaths[0]
-        console.log('in method!')
-        console.log(this.sortedPaths)
-        console.log(this.currentPath)
-        // if there's no 'next' path, return the first path
-        const nextPath = isNull(this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]) ? firstBlogPath : this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]
-        function isNull(item) {
-          return item === null || item === undefined
-        }
-        console.log(this.sortedPaths)
-        return nextPath
-      }
     },
     // get the slug as a param to import the correct md file
     async asyncData({ params }) {
@@ -67,8 +50,6 @@ const firstBlogPath = this.sortedPaths[0]
         const posts =  allPosts.keys().map((key) => {
           return allPosts(key)
         });
-        console.log(allPosts.keys())
-        console.log(posts)
         const sortedPosts = posts.sort((a,b) => {
           const dateA = new Date(a.attributes.date);
           const dateB = new Date(b.attributes.date);
@@ -80,16 +61,15 @@ const firstBlogPath = this.sortedPaths[0]
           }
             return 0;
         })
-        console.log(sortedPosts)
         const sortedPaths = [] 
         sortedPosts.map(post => {
-          console.log(post.attributes._meta.resourcePath)
-          // clean up the path
-          const relPath = post.attributes._meta.resourcePath
+          // clean up the path - split by /
+          let relPath = post.attributes._meta.resourcePath.split('/')
+          // get the end of the path, remove '.md'
+          relPath = relPath[relPath.length - 1].slice(0, -3)
           console.log(relPath)
           sortedPaths.push(relPath)
         })
-        console.log(sortedPaths)
         return {
           sortedPaths,
           post,
@@ -102,3 +82,6 @@ const firstBlogPath = this.sortedPaths[0]
     }, 
   }
 </script>
+
+//creat a regex that captures the characters between the last / and .md at the end 
+//or i could try to just capture anything up until content/...then i am in control of that path
