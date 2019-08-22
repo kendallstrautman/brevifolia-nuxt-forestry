32a20a7919bf3469fc473d4fb607e0e97a1aeef8
kendallstrautman <kendallstrautman@gmail.com>
2019-08-22 13:44:41 -0700

    debugging next routing

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -12,7 +12,7 @@
     <div class="blog__body" v-html="post.html"></div>
     <div class="blog__footer">
       <h2 >Written By: {{ post.attributes.author }}</h2>
-      <NuxtLink :to="nextBlogPath">
+      <NuxtLink :to="`/blog/${nextBlogPath}`">
         <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" x="0px" y="0px" viewBox="0 0 26 26" enableBackground="new 0 0 26 26" >
           <path d="M23.021,12.294l-8.714-8.715l-1.414,1.414l7.007,7.008H2.687v2h17.213l-7.007,7.006l1.414,1.414l8.714-8.713  C23.411,13.317,23.411,12.685,23.021,12.294z"/>
         </svg>
@@ -29,18 +29,21 @@
       }, 
       nextBlogPath() {
         const firstBlogPath = this.sortedPaths[0]
-        const currentPath = this.$nuxt._route.path
+        console.log(this.currentPath)
         // if there's no 'next' path, return the first path
-        const nextPath = isNull(this.sortedPaths[this.sortedPaths.indexOf(currentPath) + 1]) ? firstBlogPath : this.sortedPaths[this.sortedPaths.indexOf(currentPath) + 1]
+        const nextPath = isNull(this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]) ? firstBlogPath : this.sortedPaths[this.sortedPaths.indexOf(this.currentPath) + 1]
         function isNull(item) {
           return item === null || item === undefined
         }
+        console.log(this.sortedPaths)
+        console.log(this.nextPath)
         return nextPath
       } 
     },
     // get the slug as a param to import the correct md file
     async asyncData({ params }) {
       try {
+        const currentPath = params.slug
         // get current post data
         const post = await import(`~/content/blog-posts/${params.slug}.md`);
         // get all post data for next route
@@ -62,12 +65,13 @@
         const sortedPaths = [] 
         sortedPosts.map(post => {
           // clean up the path
-          const relPath = `/blog${post.attributes._meta.resourcePath.slice(85, -3)}`
+          const relPath = post.attributes._meta.resourcePath.slice(86, -3)
           sortedPaths.push(relPath)
         })
         return {
           sortedPaths,
           post,
+          currentPath
         }
       } catch(err) {
         console.debug(err)
